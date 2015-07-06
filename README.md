65196301128fbffc347ce854411a01221e257465
mgechev <mgechev@gmail.com>
2015-07-07 00:54:02 +0300

    Update readme and gulpfile

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # angular.hs
+
+In order to build you need to:
+
+```bash
+# takes quite a while...and if the magic doesn't happen...
+cabal install haste-compiler
+haste-boot
+npm install
+gulp build
+```
+
+All the output files will be located in `dist`.
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,21 +3,17 @@ import haste from 'gulp-haste';
 import watch from 'gulp-watch';
 import plumber from 'plumber';
 
-gulp.task('haste', () => {
+gulp.task('build', () => {
   'use strict';
   return gulp.src('./angular/**/*.hs')
-    .pipe(plumber({
-      errorHandler: e => {
-        console.log(e);
-      }
-    }))
     .pipe(haste())
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('watch', () => {
   'use strict';
   return watch('./angular/**/*.hs', () => {
-    gulp.start('haste');
+    return gulp.start('build');
   });
 });
+
